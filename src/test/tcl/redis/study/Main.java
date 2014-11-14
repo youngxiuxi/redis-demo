de183cf2418e7b93627a0877ce756b60286d029f
Cyang <Cyang@Cyang-THINK>
2014-11-14 14:23:50 +0800

    创建工程

---
+++ src/test/tcl/redis/study/Main.java
@@ -0,0 +1,11 @@
+package test.tcl.redis.study;
+
+import com.tcl.redis.study.RedisClient;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new RedisClient().show(); 
+    }
+
+}
